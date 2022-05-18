12bd44fbc0cc0fa1d195591a1ded42a473ed0c34
Luan Thomaz <luan.abrantes1999@gmail.com>
2022-05-18 12:10:47 -0300

    finalização da pagina com versão responsiva, e finalização do Javascript para relacionar o menu

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,6 +5,41 @@ window.addEventListener('scroll', onScroll);
 function onScroll() {
     showNavOnScroll()
     showBackToTopButtonOnScroll()
+
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section){
+    const targetLine = scrollY + innerHeight / 2
+
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+    // console.log('Chegou ou passou da linha? ', sectionTopReachOrPassedTargetLine)
+
+
+    const sectionEndsAt = sectionTop + sectionHeight
+
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+    // console.log('o fundo da seção passou da linha? ', sectionEndPassedTargetLine
+    // )
+
+    const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+
+    menuElement.classList.remove('active')
+    if(sectionBoundaries) {
+        menuElement.classList.add('active')
+    }
 }
 
 function showNavOnScroll(){
